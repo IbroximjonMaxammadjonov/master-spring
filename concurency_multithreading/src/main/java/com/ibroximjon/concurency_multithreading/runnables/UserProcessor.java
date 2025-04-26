4d1f0fe7ae4ce2782eb31b641972d64985512472
Ibroximjon <makhammadjonovibrokhimjon@gmail.com>
2025-04-26 17:07:14 +0500

    adds UserProcessor and implements call method

---
+++ concurency_multithreading/src/main/java/com/ibroximjon/concurency_multithreading/runnables/UserProcessor.java
@@ -0,0 +1,34 @@
+package com.ibroximjon.concurency_multithreading.runnables;
+
+import com.ibroximjon.concurency_multithreading.beans.User;
+import com.ibroximjon.concurency_multithreading.dao.UserDao;
+
+import java.util.StringTokenizer;
+import java.util.concurrent.Callable;
+
+public class UserProcessor implements Callable<Integer> {
+
+    private String userRecord;
+    private UserDao userDao;
+
+    UserProcessor(String userRecord, UserDao userDao) {
+        this.userRecord = userRecord;
+        this.userDao = userDao;
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        int rows = 0;
+        System.out.println(Thread.currentThread().getName() + " processing record for: " + userRecord);
+        StringTokenizer tokenizer = new StringTokenizer(userRecord,",");
+        User user = null;
+        while (tokenizer.hasMoreTokens()) {
+            user = new User();
+            user.setName(tokenizer.nextToken());
+            user.setId(Integer.parseInt(tokenizer.nextToken()));
+            user.setEmail(tokenizer.nextToken());
+            rows = userDao.saveUser(user);
+        }
+        return rows;
+    }
+}
