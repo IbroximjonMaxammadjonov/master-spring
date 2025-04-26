1c96d0d114d70b9fcd5b9379db76ae7367b409f7
Ibroximjon <makhammadjonovibrokhimjon@gmail.com>
2025-04-26 16:41:47 +0500

    test with Executor and Executors.newCachedThreadPool

--- concurency_multithreading/src/test/java/com/ibroximjon/concurency_multithreading/TestRunnable.java
+++ concurency_multithreading/src/test/java/com/ibroximjon/concurency_multithreading/TestRunnable.java
@@ -3,13 +3,18 @@ package com.ibroximjon.concurency_multithreading;
 import com.ibroximjon.concurency_multithreading.runnables.AppRunnable;
 
 import java.io.*;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class TestRunnable {
 
     public static void main(String[] args) {
         Runnable runnable = new AppRunnable();
-        runnable.run();
+        /*runnable.run();
         Thread thread = new Thread(runnable);
-        thread.start();
+        thread.start();*/
+
+        Executor executor = Executors.newCachedThreadPool();
+        executor.execute(runnable);
     }
 }
